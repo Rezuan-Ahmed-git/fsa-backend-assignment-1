0726d7d6fcfcc89faae3d0786b7fd935e3b2e48c
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2023-05-13 21:49:54 +0600

    fix bug

--- src/app.js
+++ src/app.js
@@ -6,11 +6,15 @@ const app = express();
 app.get('/api/random-number', (req, res) => {
   const startValue = parseInt(req.query.start);
   const endValue = parseInt(req.query.end);
-  res.json({
-    startValue,
-    endValue,
-    randomNumber: generateRandomNumber(startValue, endValue),
-  });
+  try {
+    res.json({
+      startValue,
+      endValue,
+      randomNumber: generateRandomNumber(startValue, endValue),
+    });
+  } catch {
+    res.status(422).json({ message: 'Invalid query param' });
+  }
 });
 
 app.listen(4000, () => {
